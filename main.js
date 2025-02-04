e068ccaf6a5dee40895eddc006d80de17e5271d3
xuannn0915 <xuannn0915@gmail.com>
2025-02-04 18:55:24 +0800

    home loading effect

--- main.js
+++ main.js
@@ -3,10 +3,30 @@ import 'bootstrap/dist/js/bootstrap.min.js';
 
 // loading
 document.addEventListener("DOMContentLoaded", function () {
+  let content = document.querySelector(".loading-content");
+  
+  // 頁面載入時先淡出網站內容
+  content.style.opacity = "0";
+
   setTimeout(() => {
-    document.querySelector(".logo-loading").style.display = "none";
-  }, 2300); // 確保動畫結束後才隱藏
+    // 顯示 Loading 畫面
+    document.querySelector(".logo-loading").style.opacity = "1";
+    content.style.visibility = "hidden"; // 防止頁面閃現
+
+    setTimeout(() => {
+      // Loading 淡出
+      document.querySelector(".logo-loading").style.opacity = "0";
+      setTimeout(() => {
+        document.querySelector(".logo-loading").style.display = "none";
+        // 頁面淡入
+        content.style.visibility = "visible";
+        content.style.opacity = "1";
+      }, 500); // 確保 Loading 淡出後才顯示頁面
+    }, 1000); // Loading 顯示 2.3 秒
+  }, 500); // 先讓原本的內容淡出後才進入 Loading
 });
+
+
 
 
 
